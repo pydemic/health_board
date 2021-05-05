c2d722920e0442f14763b903e2383afb131c6b11
Laercio Silva <laerciossjr@gmail.com>
2021-05-04 21:58:33 -0300

    Adding csvtool in extractor

--- lib/health_board/updaters/morbidities/flu_syndrome_updater/extractor.ex
+++ lib/health_board/updaters/morbidities/flu_syndrome_updater/extractor.ex
@@ -11,8 +11,9 @@ defmodule HealthBoard.Updaters.FluSyndromeUpdater.Extractor do
     "profissionalSaude",
     "resultadoTeste",
     "evolucaoCaso",
+    "dataEncerramento",
     "classificacaoFinal",
-    "dataEncerramento"
+    "dataNotificacao"
   ]
 
   @spec extract(String.t(), String.t()) :: :ok
@@ -23,177 +24,82 @@ defmodule HealthBoard.Updaters.FluSyndromeUpdater.Extractor do
     File.mkdir_p!(output_path)
 
     output_file = File.open!(output_file_path, [:write, :append, :utf8])
+
     IO.binwrite(output_file, NimbleCSV.RFC4180.dump_to_iodata([@headers]))
+
+    File.close(output_file)
 
     input_path
     |> File.ls!()
-    |> Enum.each(&extract_file(Path.join(input_path, &1), output_file))
-
-    File.close(output_file)
+    |> Enum.each(&extract_file(Path.join(input_path, &1), output_file_path))
 
     finish_extraction(output_file_path)
 
     :ok
   end
 
-  defp extract_file(input_file_path, output_file) do
-    input_file_path
-    |> File.stream!(read_ahead: 100_000, modes: [:read_ahead, {:encoding, :latin1}])
-    |> NimbleCSV.Semicolon.parse_stream()
-    |> Stream.map(&extract_line_data/1)
-    |> NimbleCSV.RFC4180.dump_to_iodata()
-    |> write(output_file)
+  defp extract_file(input_file_path, output_file_path) do
+    filename = Path.basename(input_file_path)
+
+    temporary_path = "/tmp/flu_syndrome_extractor_temporary"
+
+    temporary_file_path = Path.join(temporary_path, filename)
+
+    File.rm_rf!(temporary_path)
+    File.mkdir_p!(temporary_path)
+
+    fix_enconding_file(input_file_path, temporary_file_path)
+
+    remove_header(temporary_file_path)
+
+    extract_data(temporary_file_path, output_file_path)
+
+    File.rm_rf!(temporary_path)
   end
 
-  defp extract_line_data(line) do
-    line_length = length(line)
+  defp fix_enconding_file(input_file_path, temporary_file_path) do
+    {_result, 0} =
+      System.cmd("sh", ["-c", "iconv -f windows-1252 -t utf-8 " <> input_file_path <> " > " <> temporary_file_path])
+  end
+
+  defp remove_header(temporary_file_path) do
+    {_result, 0} =
+      System.cmd("sh", [
+        "-c",
+        "csvtool drop 1 " <> temporary_file_path <> " > temp.csv && mv temp.csv " <> temporary_file_path
+      ])
+  end
+
+  defp extract_data(temporary_file_path, output_file_path) do
+    {line_length, 0} =
+      System.cmd("sh", [
+        "-c",
+        "head -n 1 " <> temporary_file_path <> " | awk -F ';' '{print NF}'"
+      ])
 
     case line_length do
-      29 ->
-        [
-          _id,
-          _notification_datetime,
-          symptoms_datetime,
-          birth_date,
-          _symptoms,
-          is_health_professional,
-          _cbo,
-          _conditions,
-          _test_status,
-          _test_date,
-          _test_type,
-          test_result,
-          _origin_country,
-          gender,
-          _residence_state,
-          _residence_state_id,
-          _residence_city,
-          residence_city_id,
-          _origin,
-          _notification_state,
-          _notification_state_id,
-          _notification_city,
-          _notification_city_id,
-          _removed,
-          _validated,
-          age,
-          final_date,
-          case_evolution,
-          final_classification
-        ] = line
+      "29\n" ->
+        slice_file(
+          temporary_file_path,
+          output_file_path,
+          "'%(18),%(3),%(4),%(26),%(14),%(6),%(12),%(28),%(27),%(29),%(2)\n'"
+        )
 
-        [
-          residence_city_id,
-          symptoms_datetime,
-          birth_date,
-          age,
-          gender,
-          is_health_professional,
-          test_result,
-          case_evolution,
-          final_classification,
-          final_date
-        ]
-
-      30 ->
-        [
-          _id,
-          _notification_datetime,
-          symptoms_datetime,
-          birth_date,
-          _symptoms,
-          is_health_professional,
-          _cbo,
-          _conditions,
-          _test_status,
-          _test_date,
-          _test_type,
-          test_result,
-          _origin_country,
-          gender,
-          _residence_state,
-          _residence_state_id,
-          _residence_city,
-          residence_city_id,
-          _origin,
-          _cnes,
-          _notification_state,
-          _notification_state_id,
-          _notification_city,
-          _notification_city_id,
-          _removed,
-          _validated,
-          age,
-          final_date,
-          case_evolution,
-          final_classification
-        ] = line
-
-        [
-          residence_city_id,
-          symptoms_datetime,
-          birth_date,
-          age,
-          gender,
-          is_health_professional,
-          test_result,
-          case_evolution,
-          final_classification,
-          final_date
-        ]
-
-      31 ->
-        [
-          _id,
-          _notification_datetime,
-          symptoms_datetime,
-          birth_date,
-          _symptoms,
-          is_health_professional,
-          _cbo,
-          _conditions,
-          _conditions_2,
-          _conditions_3,
-          _test_status,
-          _test_date,
-          _test_type,
-          test_result,
-          _origin_country,
-          gender,
-          _residence_state,
-          _residence_state_id,
-          _residence_city,
-          residence_city_id,
-          _origin,
-          _notification_state,
-          _notification_state_id,
-          _notification_city,
-          _notification_city_id,
-          _removed,
-          _validated,
-          age,
-          final_date,
-          case_evolution,
-          final_classification
-        ] = line
-
-        [
-          residence_city_id,
-          symptoms_datetime,
-          birth_date,
-          age,
-          gender,
-          is_health_professional,
-          test_result,
-          case_evolution,
-          final_classification,
-          final_date
-        ]
+      "30\n" ->
+        slice_file(
+          temporary_file_path,
+          output_file_path,
+          "'%(18),%(3),%(4),%(26),%(14),%(6),%(12),%(28),%(27),%(29),%(2)\n'"
+        )
     end
   end
 
-  defp write(content, file) do
-    IO.binwrite(file, content)
+  defp slice_file(input_file_path, output_file_path, structure) do
+    {_result, 0} =
+      System.cmd("sh", [
+        "-c",
+        "csvtool -t ';' format " <> structure <> " " <> input_file_path <> " >> " <> output_file_path
+      ])
   end
 
   defp finish_extraction(file_path) do
